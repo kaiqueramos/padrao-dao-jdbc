b02b57317fd981fe57fbb66a1c961d353c8ad556
kaiqueramos <kaiqueramospessoal@gmail.com>
2020-09-29 19:53:29 -0300

    SellerDaoJDBC, DepartmentDaoJDBC and DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import model.dao.impl.DepartmentDaoJDBC;
+import model.dao.impl.SellerDaoJDBC;
+import model.entities.Seller;
+
+public class DaoFactory {
+    public static SellerDao createSellerDao(){
+        return new SellerDaoJDBC();
+    }
+    public static DepartmentDao createDepartmentDao(){
+        return new DepartmentDaoJDBC();
+    }
+}
