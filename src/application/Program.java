1063b94cc6a09e6fc99c7dad5d498abb19bb7d1e
kaiqueramos <kaiqueramospessoal@gmail.com>
2020-10-01 23:15:41 -0300

    Add comment

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,12 +14,6 @@ public class Program {
     public static void main(String[] args) {
         SellerDao sl = DaoFactory.createSellerDao();
         DepartmentDao dp = DaoFactory.createDepartmentDao();
-
-        Department dep = new Department();
-        dep.setName("Foods and grocery");
-        dep.setId(10);
-
-        List<Department> list = dp.findAll();
-        list.forEach(System.out::println);
+        //As operações estão disponíveis a partir dos objetos acima.
     }
 }
